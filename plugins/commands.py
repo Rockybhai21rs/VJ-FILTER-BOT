b37aa19c2247891c704577ba6a54334e3fbba882
Rockybhai21rs <rockingstar.21rs@gmail.com>
2025-02-08 23:53:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -189,7 +189,7 @@ async def start(client, message):
                     await client.send_message(chat_id = user_id, text = "<b>You Have Successfully Completed Total Referal.\n\nYou Added In Premium For {}</b>".format(REFERAL_PREMEIUM_TIME))
                     return 
         else:
-            if 💳 PREMIUM_AND_REFERAL_MODE ⚜== True:
+            if PREMIUM_AND_REFERAL_MODE == True:
                 buttons = [[
                     InlineKeyboardButton('💕 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 💖', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
